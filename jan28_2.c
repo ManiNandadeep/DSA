ceda522a8e958ca9f33d4a925fdb0b9a3e7e7a85
ManiNandadeep <mani.nandadeep@iiitb.org>
2020-01-28 17:14:20 +0530

    Update jan28_2.c

--- jan28_2.c
+++ jan28_2.c
@@ -1,5 +1,38 @@
 #include<stdio.h>
 #include<stdbool.h>
+#include<string.h>
+#include<stdlib.h>
+#define swap(x,y) int t=x;x=y;y=t;
+int partition(int arr[],int i,int j,int pivot)
+{
+    int l=i;
+    int r=j;
+    while(l<=r){
+        while(l<=r && arr[l]<=pivot) 
+            l++;
+        while(l<=r && arr[r]>pivot) 
+            r--;
+        if(l<=r) {
+            swap(arr[l],arr[r]);
+            l++; r--;
+        }
+    }
+    int k = l-1;
+    swap(arr[i-1],arr[k]);
+    arr[k] = pivot;
+    return k;
+}
+void Rquicksort(int arr[],int i,int j)
+{
+    if(i<j)
+    {
+        int pivot_index=rand()%(j-i+1)+i;
+        swap(arr[i],arr[pivot_index]);
+        int k=partition(arr,i+1,j,arr[i]);
+        Rquicksort(arr,i,k-1);
+        Rquicksort(arr,k+1,j);
+    }
+}
 bool func(int arr[],int l,int r,int x)
 {
     while(l<=r)
@@ -18,6 +51,7 @@ int main(void)
     for(int i=0;i<n;i++)
         scanf("%d ",&arr[i]);
     scanf("%d",&x);
+    Rquicksort(arr,0,n-1);
     int value=func(arr,0,n-1,x);
     printf("%d",value);
     return 0;
