4d2aee882c877810f2be92e7517a58ee9a2ba24c
ManiNandadeep <mani.nandadeep@iiitb.org>
2020-04-16 21:30:28 +0530

    Update validate_BST.c

--- binarytree/binary_search_tree/validate_BST.c
+++ binarytree/binary_search_tree/validate_BST.c
@@ -9,7 +9,8 @@ class Solution{
     {
       if(root==NULL) return true;
       if(!validate(root->left,prev)) return false;
-      if(prev!=NULL && prev->val >=node->val) return false;
+      if(prev!=NULL && prev->val >=root->val) return false;
+      prev=root;
       return validate(root->right,prev);
-    }
-}
+    } 
+};
